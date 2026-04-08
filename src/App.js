ae960dcd0a7d7a5538fed7e0932e7a7e17ed51f5
Uchqunbek12 <uchqunbek729@gmail.com>
2026-04-08 11:42:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+mport React, { useState, useCallback } from 'react';
 import * as XLSX from "xlsx";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, BarChart, Bar, Cell, Legend, CartesianGrid, PieChart, Pie } from "recharts";
 
