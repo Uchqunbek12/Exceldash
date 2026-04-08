056694ecc09054a0db319d370625784c5182801b
Uchqunbek12 <uchqunbek729@gmail.com>
2026-04-08 11:40:41 +0500

    Refactor import statement for React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
+import React, { useState } from 'react';
 import * as XLSX from "xlsx";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, BarChart, Bar, Cell, Legend, CartesianGrid, PieChart, Pie } from "recharts";
 
